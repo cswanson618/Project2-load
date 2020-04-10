4315562edc70b3379378af6acfaa827893ea478f
cswanson618 <54300477+cswanson618@users.noreply.github.com>
2020-04-10 13:41:22 -0500

    Ran Black

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-import pandas as pd 
-import requests 
+import pandas as pd
+import requests
 from sqlalchemy import create_engine
 import datetime
 import country_converter as coco
@@ -9,32 +9,65 @@ import sqlalchemy
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def load():
     ### World Data
     # Covid-19 Confirmed Cases
     confirmed_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-    confirmed_html = requests.get(confirmed_url).text 
+    confirmed_html = requests.get(confirmed_url).text
     confirmed_df = pd.read_html(confirmed_html)[0]
     confirmed_df = confirmed_df.iloc[:, 1:]
-    confirmed_df = confirmed_df.melt(id_vars=['Country/Region', 'Province/State', 'Lat', 'Long'])
-    confirmed_df = confirmed_df.rename(columns={"Country/Region": "country_region", "Province/State": "province_state",  "Lat": "lat", "Long": "long", "variable":"date", "value": "confirmed"})
+    confirmed_df = confirmed_df.melt(
+        id_vars=["Country/Region", "Province/State", "Lat", "Long"]
+    )
+    confirmed_df = confirmed_df.rename(
+        columns={
+            "Country/Region": "country_region",
+            "Province/State": "province_state",
+            "Lat": "lat",
+            "Long": "long",
+            "variable": "date",
+            "value": "confirmed",
+        }
+    )
 
     # Covid-19 Deaths
     deaths_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
-    deaths_html = requests.get(deaths_url).text 
+    deaths_html = requests.get(deaths_url).text
     deaths_df = pd.read_html(deaths_html)[0]
     deaths_df = deaths_df.iloc[:, 1:]
-    deaths_df = deaths_df.melt(id_vars=['Country/Region', 'Province/State', 'Lat', 'Long'])
-    deaths_df = deaths_df.rename(columns={"Country/Region": "country_region", "Province/State": "province_state", "Lat": "lat", "Long": "long", "variable":"date", "value": "deaths"})
+    deaths_df = deaths_df.melt(
+        id_vars=["Country/Region", "Province/State", "Lat", "Long"]
+    )
+    deaths_df = deaths_df.rename(
+        columns={
+            "Country/Region": "country_region",
+            "Province/State": "province_state",
+            "Lat": "lat",
+            "Long": "long",
+            "variable": "date",
+            "value": "deaths",
+        }
+    )
 
     # Covid-19 Recovered
     recovered_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
-    recovered_html = requests.get(recovered_url).text 
+    recovered_html = requests.get(recovered_url).text
     recovered_df = pd.read_html(recovered_html)[0]
     recovered_df = recovered_df.iloc[:, 1:]
-    recovered_df = recovered_df.melt(id_vars=['Country/Region', 'Province/State', 'Lat', 'Long'])
-    recovered_df = recovered_df.rename(columns={"Country/Region": "country_region", "Province/State": "province_state", "Lat": "lat", "Long": "long", "variable":"date", "value": "recovered"})
+    recovered_df = recovered_df.melt(
+        id_vars=["Country/Region", "Province/State", "Lat", "Long"]
+    )
+    recovered_df = recovered_df.rename(
+        columns={
+            "Country/Region": "country_region",
+            "Province/State": "province_state",
+            "Lat": "lat",
+            "Long": "long",
+            "variable": "date",
+            "value": "recovered",
+        }
+    )
 
     # Merge three dataframes
     covid_merge1 = pd.merge(confirmed_df, deaths_df, how="outer")
@@ -61,16 +94,26 @@ def load():
     iso_url = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv"
     iso_html = requests.get(iso_url).text
     iso_df = pd.read_html(iso_html)[0]
-    iso3_df = iso_df[["iso3", "Country_Region", "Province_State"]].rename(columns={"Country_Region": "country_region", "Province_State": "province_state"})
+    iso3_df = iso_df[["iso3", "Country_Region", "Province_State"]].rename(
+        columns={"Country_Region": "country_region", "Province_State": "province_state"}
+    )
     iso3_df = iso3_df.loc[iso3_df["province_state"].isnull()].drop("province_state", 1)
     old_pop_df = pd.read_csv("older_pop_2018.csv")
 
-    covid_df2 = covid_df[["country_region", "date", "province_state", "confirmed", "deaths", "recovered"]]
+    covid_df2 = covid_df[
+        ["country_region", "date", "province_state", "confirmed", "deaths", "recovered"]
+    ]
     covid_df2 = covid_df2.groupby(["country_region", "date"]).sum().reset_index()
-    covid_df2["case_fatality"] = round(covid_df2["deaths"] / covid_df2["confirmed"] * 100, 2)
+    covid_df2["case_fatality"] = round(
+        covid_df2["deaths"] / covid_df2["confirmed"] * 100, 2
+    )
     covid_df3 = pd.merge(covid_df2, iso3_df, how="left")
     plot_df = pd.merge(covid_df3, old_pop_df, how="left")
-    plot_df = plot_df.loc[plot_df["date"] >= datetime.date(2020,3,1)].reset_index().drop("index", axis=1)
+    plot_df = (
+        plot_df.loc[plot_df["date"] >= datetime.date(2020, 3, 1)]
+        .reset_index()
+        .drop("index", axis=1)
+    )
 
     # Connect to the "Covid" database
 
@@ -82,12 +125,8 @@ def load():
     plot_df.to_sql(con=engine, name="plotting", if_exists="replace")
 
     s = "Successful"
-    return(s)
+    return s
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
-
-
-
+    app.run()
